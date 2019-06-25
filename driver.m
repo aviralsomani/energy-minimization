9be20c3ba33b5f7af86be081f61dc11166222cfb
Aviral Somani <avisomani@Gyanada.local>
2019-06-25 13:33:03 -0500

    First Commit

---
+++ driver.m
@@ -0,0 +1,5 @@
+P = rand(10000, 3)
+tic
+e = energy(P, 2)
+toc
+print(e)
---
+++ energy.m
@@ -0,0 +1,13 @@
+function e = energy(points, s)
+    p = -1 * s;
+    n = size(points, 1);
+    sum = 0;
+    for i = 1:n-1
+        for j = (i + 1):n
+            no = norm(points(j, :) - points(i, :));
+            sum = sum + power(no, p);
+        end
+    end
+    e = sum;
+end
+
