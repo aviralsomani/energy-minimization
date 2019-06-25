c560c7ffa7aa6591ce2dfa21659d386db4f00994
Aviral Somani <avisomani@Gyanada.local>
2019-06-25 14:05:15 -0500

    Suppressed output

--- driver.m
+++ driver.m
@@ -4,7 +4,7 @@ e = energy(P, 2)
 toc
 sizes = [100, 500, 1000, 5000, 10000]
 for i = 1:5
-    A = rand(sizes(i), 3)
+    A = rand(sizes(i), 3);
     fprintf("Trial %d:", i)
     tic
     del = gradient(A, 2)
