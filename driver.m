58d99dd6fb499ea965bde6151bdc044354382e7f
Aviral Somani <avisomani@Gyanada.local>
2019-06-25 14:04:07 -0500

    Benchmark for gradient

--- driver.m
+++ driver.m
@@ -2,3 +2,11 @@ P = rand(10000, 3);
 tic
 e = energy(P, 2)
 toc
+sizes = [100, 500, 1000, 5000, 10000]
+for i = 1:5
+    A = rand(sizes(i), 3)
+    fprintf("Trial %d:", i)
+    tic
+    del = gradient(A, 2)
+    toc
+end
