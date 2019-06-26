24f57e01e112ed0d7a40fdd43a5901b133a8bfe8
Aviral Somani <avisomani@Gyanada.local>
2019-06-26 15:26:19 -0500

    Increasing values of test

--- src_matlab/knnDriver.m
+++ src_matlab/knnDriver.m
@@ -1,5 +1,5 @@
 s = [10000, 15000, 20000, 25000]
-k = [4, 8, 16, 32, 64]
+k = [32, 64, 128, 256, 512]
 fprintf("Trial with size of point configurations (k = 5)\n")
 % for l = 1:4
 %     fprintf("Trial with %d points: ", s(l))
@@ -8,8 +8,8 @@ fprintf("Trial with size of point configurations (k = 5)\n")
 %     e = energy(KNNDist(A, 5), 2);
 %     toc
 % end
-fprintf("Trial with differing values of \'k\' (n = 5000) \n")
-A = rand(5000, 3);
+fprintf("Trial with differing values of \'k\' (n = 10000) \n")
+A = rand(10000, 3);
 for l = 1:5
     fprintf("Trial %d (k = %d): ", l, k(l))
     tic
