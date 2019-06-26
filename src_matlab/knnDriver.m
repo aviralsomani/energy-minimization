b0ac069e14c2b95352c9f44e5729a227d1884fae
Aviral Somani <avisomani@Gyanada.local>
2019-06-26 14:15:28 -0500

    Increasing Test sizes

--- src_matlab/knnDriver.m
+++ src_matlab/knnDriver.m
@@ -1,5 +1,5 @@
 k = [3, 4, 5, 6, 7]
-s = [1000, 2000, 3000, 4000, 5000]
+s = [10000, 15000, 20000, 25000]
 for l = 1:5
     fprintf("Trial with %d points: \n", s(l))
     for j = 1:5
@@ -7,11 +7,14 @@ for l = 1:5
         fprintf("\tTrial %d for energy with KNN (k=%d): \n", j, k(j))
         fprintf("\t - KNNDist Time: ")
         tic
+        tic
         D = KNNDist(A, k(j));
         toc
         fprintf("\t - Energy Time: ")
         tic
         e = energy(KNNDist(A, k(j)), 2);
+        toc
+        fprintf("\t - Total Time: ")
         toc
     end
 end
