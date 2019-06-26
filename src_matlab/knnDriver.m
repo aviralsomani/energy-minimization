4631d208f1162a1c35a4c65036b2acf6cb744c4c
Aviral Somani <avisomani@Gyanada.local>
2019-06-26 14:45:16 -0500

    Cleaned up KNN testing driver

--- src_matlab/knnDriver.m
+++ src_matlab/knnDriver.m
@@ -1,20 +1,8 @@
-k = [3, 4, 5, 6, 7]
 s = [10000, 15000, 20000, 25000]
 for l = 1:5
     fprintf("Trial with %d points: \n", s(l))
-    for j = 1:5
-        A = rand(s(l), 3);
-        fprintf("\tTrial %d for energy with KNN (k=%d): \n", j, k(j))
-        fprintf("\t - KNNDist Time: ")
-        tic
-        tic
-        D = KNNDist(A, k(j));
-        toc
-        fprintf("\t - Energy Time: ")
-        tic
-        e = energy(KNNDist(A, k(j)), 2);
-        toc
-        fprintf("\t - Total Time: ")
-        toc
-    end
+    A = rand(s(l), 3);
+    tic
+    e = energy(KNNDist(A, 5), 2);
+    toc
 end
