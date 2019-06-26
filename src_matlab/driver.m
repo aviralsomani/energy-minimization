e2b9bdccea6bbd2c021f2fd0557d0f3585b8e872
Aviral Somani <avisomani@Gyanada.local>
2019-06-26 12:55:52 -0500

    removed gradient benchmark

--- src_matlab/driver.m
+++ src_matlab/driver.m
@@ -1,11 +1,11 @@
-sizes = [5000, 10000, 15000, 20000]
-for j = 1:4
-    A = rand(sizes(j), 3);
-    fprintf("Trial %d for gradient: ", j)
-    tic
-    del = gradient(A, 2);
-    toc
-end
+% sizes = [5000, 10000, 15000, 20000]
+% for j = 1:4
+%     A = rand(sizes(j), 3);
+%     fprintf("Trial %d for gradient: ", j)
+%     tic
+%     del = gradient(A, 2);
+%     toc
+% end
 k = [3, 4, 5, 6, 7]
 for j = 1:5
     A = rand(10000, 3);
