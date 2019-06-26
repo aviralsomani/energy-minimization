8684200efe10e29924c569f89c2e049c21b74bda
Aviral Somani <avisomani@Gyanada.local>
2019-06-26 14:46:01 -0500

    Small additional style fix

--- src_matlab/knnDriver.m
+++ src_matlab/knnDriver.m
@@ -1,6 +1,6 @@
 s = [10000, 15000, 20000, 25000]
 for l = 1:5
-    fprintf("Trial with %d points: \n", s(l))
+    fprintf("Trial with %d points: ", s(l))
     A = rand(s(l), 3);
     tic
     e = energy(KNNDist(A, 5), 2);
