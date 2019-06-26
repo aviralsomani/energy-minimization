d6e0e8725ecc40371b19f147bdadf4d95dcb9752
Aviral Somani <avisomani@Gyanada.local>
2019-06-26 15:24:37 -0500

    Cleaning up benchmark output

--- src_matlab/knnDriver.m
+++ src_matlab/knnDriver.m
@@ -1,15 +1,15 @@
 s = [10000, 15000, 20000, 25000]
 k = [4, 8, 16, 32, 64]
-fprintf("Trial with size of point configurations\n")
-for l = 1:4
-    fprintf("Trial with %d points: ", s(l))
-    A = rand(s(l), 3);
-    tic
-    e = energy(KNNDist(A, 5), 2);
-    toc
-end
-fprintf("Trial with differing values of \'k\' (n = 15000) \n")
-A = rand(15000, 3);
+fprintf("Trial with size of point configurations (k = 5)\n")
+% for l = 1:4
+%     fprintf("Trial with %d points: ", s(l))
+%     A = rand(s(l), 3);
+%     tic
+%     e = energy(KNNDist(A, 5), 2);
+%     toc
+% end
+fprintf("Trial with differing values of \'k\' (n = 5000) \n")
+A = rand(5000, 3);
 for l = 1:5
     fprintf("Trial %d (k = %d): ", l, k(l))
     tic
