92487ec1226ab1f74805950fc3c0f127139e24c0
Aviral Somani <avisomani@Gyanada.local>
2019-06-27 16:19:42 -0500

    More corrections

--- src_matlab/KNNDist.m
+++ src_matlab/KNNDist.m
@@ -2,9 +2,9 @@ function D = KNNDist(P, k)
 n = size(P, 1);
 D = zeros(n, k);
 parfor j = 1:n
-    if (j == 1); dists = knnsearch(P(2:n),P1,'K',k);
+    if (j == 1); dists = knnsearch(P(2:n),P(j,:),'K',k);
     else; knnsearch([P(1:j-1,:)
-                    P(j+1:n,:)]);
+                    P(j+1:n,:)],P(j,:),'K',k);
     end
     D(j, :) = dists';
 end
