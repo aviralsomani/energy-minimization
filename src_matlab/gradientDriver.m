1dd2f9b7b45934c0fac2e657c35ad9beeac5da8f
Aviral Somani <avisomani@Gyanada.local>
2019-06-27 14:32:09 -0500

    Updated benchmarks to include vectorized gradient

--- src_matlab/gradientDriver.m
+++ src_matlab/gradientDriver.m
@@ -1,8 +1,15 @@
 sizes = [5000, 10000, 15000, 20000]
 for j = 1:4
     A = rand(sizes(j), 3);
-    fprintf("Trial %d for gradient: ", j)
+    fprintf("Trial %d for parfor gradient: ", j)
     tic
     del = gradient(A, 2);
+    toc
+end
+for j = 1:4
+    A = rand(sizes(j), 3);
+    fprintf("Trial %d for vectorized gradient: ", j)
+    tic
+    del = vGradient(A, 2);
     toc
 end
