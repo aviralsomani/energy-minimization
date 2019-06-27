e7b18dce0dbdcb66907ee498e27ab29b1023a103
Aviral Somani <avisomani@Gyanada.local>
2019-06-27 16:24:43 -0500

    more bugs fixed

--- src_matlab/KNNDist.m
+++ src_matlab/KNNDist.m
@@ -1,10 +1,10 @@
 function D = KNNDist(P, k)
 n = size(P, 1);
 D = zeros(n, k);
-dists = zeros(n-1, k)
+dists = zeros(k, 1)
 parfor j = 1:n
-    if (j == 1); dists = knnsearch(P(2:n),P(j,:),'K',k);
-    else; dists = knnsearch([P(1:j-1,:)
+    if (j == 1); [~,dists] = knnsearch(P(2:n),P(j,:),'K',k);
+    else; [~,dists] = knnsearch([P(1:j-1,:)
                     P(j+1:n,:)],P(j,:),'K',k);
     end
     D(j, :) = dists';
