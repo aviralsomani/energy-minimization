1373871c40093dcde3ef2e203f7d8f3aff6d188b
Aviral Somani <avisomani@Gyanada.local>
2019-07-03 14:01:26 -0500

    Checking on vectorized gradient again

--- src_matlab/optimizeEnergy.m
+++ src_matlab/optimizeEnergy.m
@@ -3,7 +3,14 @@ function [P_next, en, pg_ratio, err, alph] = optimizeEnergy(Pfn, k, s, alph, eps
     en = energy(P, 2);
     step = 1;
     pg_ratio = 1;
+    fprintf("Step %d", step);
     while (step <= maxstep && pg_ratio >= prop)
+        if step > 1
+            for j = 1:(log10(step + 1))
+                fprintf("\b");
+            end
+        end
+        fprintf("%d", step);
         [G] = gradient(P, s);
         Proj = step_S2(P, G);
         L1Proj_sum = sum(vecnorm(Proj, 2, 2));
@@ -19,6 +26,5 @@ function [P_next, en, pg_ratio, err, alph] = optimizeEnergy(Pfn, k, s, alph, eps
         if pg_ratio > 0.1 && err/alph > eps
             alph = alph/1.005;
         end
-        fprintf("Step %d Complete\n", step);
     end
 end
--- src_matlab/vGradient.m
+++ src_matlab/vGradient.m
@@ -2,13 +2,13 @@ function [del] = vGradient(P, s)
 n = size(P, 1);
 del = zeros(size(P));
 val = zeros(size(P));
-parfor i = 1:n
+for i = 1:n
     val = zeros(size(P));
     cur = P(i, :);
     val = -1 * P + cur;
     norms = vecnorm(val,2,2).^(s+2);
     val = -2 * s * val;
-    val = val./norms
+    val = val./norms;
     val(i,:) = zeros(1, size(P,2));
     del(i,:) = sum(val);
 end
