2dcb3f718c4cce26e7f2db9d7e65dc7b377a965a
Aviral Somani <avisomani@Gyanada.local>
2019-07-03 16:55:29 -0500

    Commented first half of energy benchmarks

--- src_matlab/energyDriver.m
+++ src_matlab/energyDriver.m
@@ -1,11 +1,11 @@
 sizes = [50000, 60000, 70000, 80000];
-for j = 1:4
-    A = rand(sizes(j), 3);
-    fprintf("Trial %d for iterative Energy: ", j)
-    tic
-    del = oldEnergy(A, 2);
-    toc
-end
+% for j = 1:4
+%     A = rand(sizes(j), 3);
+%     fprintf("Trial %d for iterative Energy: ", j)
+%     tic
+%     del = oldEnergy(A, 2);
+%     toc
+% end
 for j = 1:4
     A = rand(sizes(j), 3);
     fprintf("Trial %d for vectorized Energy: ", j)
