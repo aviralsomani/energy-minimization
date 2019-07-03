79dbb506e10ecc7bce797c371646d256031beba8
Aviral Somani <avisomani@Gyanada.local>
2019-07-03 16:20:00 -0500

    Changing energy again

--- src_matlab/energy.m
+++ src_matlab/energy.m
@@ -5,9 +5,9 @@ function e = energy(P, s)
     parfor i = 1:n
         D(i,:) = vecnorm(P - P(i,:),2,2)';
     end
-    D = D + eye(size(D, 1));
-    %e = sum(sum(triu(D .^ p, 1)));
-    e = sum(D.^p, 'all') - n;
+%     D = D + eye(size(D, 1));
+%     e = sum(D.^p, 'all') - n;
+    e = sum(sum(triu(D .^ p, 1)));
 end
 
 % function e = energy(dists, s)
