9251ea299ae3430ade1575e61b86102de365c979
Aviral Somani <avisomani@Gyanada.local>
2019-07-03 14:09:02 -0500

    Fixed dependency issues in gradientDriver

--- src_matlab/gradientDriver.m
+++ src_matlab/gradientDriver.m
@@ -3,13 +3,13 @@ for j = 1:4
     A = rand(sizes(j), 3);
     fprintf("Trial %d for parfor gradient: ", j)
     tic
-    del = gradient(A, 2);
+    del = oldGradient(A, 2);
     toc
 end
 for j = 1:4
     A = rand(sizes(j), 3);
     fprintf("Trial %d for vectorized gradient: ", j)
     tic
-    del = vGradient(A, 2);
+    del = gradient(A, 2);
     toc
 end
