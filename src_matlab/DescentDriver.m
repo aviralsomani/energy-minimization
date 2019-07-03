86993a8b578c65dd0fe52e523885ed302b3a4a97
Aviral Somani <avisomani@Gyanada.local>
2019-07-03 10:57:17 -0500

    Preparing Descent Driver for run on cluster

--- src_matlab/DescentDriver.m
+++ src_matlab/DescentDriver.m
@@ -1,12 +1,12 @@
-n = 50;
+n = 1000;
 k = n-1;
 s = 2;
 P = normrnd(0, 1, [n 3]);
 P = normalize(P);
 plotPoints(P);
 [N, en, pg_ratio, err, alph] = optimizeEnergy(P, k, s, 0.001, 0.001, 5000, 0.0005);
-plotPoints(N);
+% plotPoints(N);
 x = 1:size(en, 1);
 y = en';
-plot(x,y);
+% plot(x,y);
 en(size(en, 1))
