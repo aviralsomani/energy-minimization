9b67ba0295379c1967be7f97f2d8441e5541aba9
Aviral Somani <avisomani@Gyanada.local>
2019-07-25 15:46:24 -0500

    Added csvwrite to output configs from cluster and suppressed plots

--- src_matlab/Manifolds/SphereDriver.m
+++ src_matlab/Manifolds/SphereDriver.m
@@ -2,12 +2,12 @@ n = 10000;
 s = 1;
 P = normrnd(0, 1, [n 3]);
 P = normalize(P);
-plotPoints(P);
+% plotPoints(P);
 
 [output, energy, pg_ratio, err, alph] = mgd(P, 50, @sphere, @gradSphere, 1, 1, 0.001, 0.001, 10000, 0.005, false);
 
-plotPoints(output);
-
+% plotPoints(output);
+csvwrite('config.csv', P)
 function y = sphere(x)
     y = x(1)^2 + x(2)^2 + x(3)^2;
 end
